37d2b760ba60a6f4102cdafc3bbe05416c41505e
PallFebrian <pallfebrian9@gmail.com>
2022-07-28 13:16:16 +0700

    hapus file belajar-komponen

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
+import React from "react";
 
-import React from 'react';
-import Header from "./Component/header";
-import Tes from "./Component/module/tes";
-import {Button,Input} from "./Component/name"
-
-function App(){
-  
-  return(
+function App() {
+  return (
     <React.Fragment>
       <h1>Latihan Export Import</h1>
-      <Header/>
-      <Tes/>
-      <Input/>
-      <Button/>
     </React.Fragment>
-  )
+  );
 }
 
 // export nama bisa 1 komponen atau lebih
