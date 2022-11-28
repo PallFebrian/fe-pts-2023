7fbf025ee792a75939d6b755449ff66fd29cf779
PallFebrian <pallfebrian9@gmail.com>
2022-11-28 12:45:29 +0700

    pas-nopal2022

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -3,9 +3,9 @@ import React from "react";
 export default function Login(){
     return(
         <React.Fragment>
-            <div>
+            <div className="w-screen h-screen bg-[#4b5ae2]">
                 <div className="bg-red-500 h-full w-[50%]">
-;l;lcm
+;l;lcmq
                 </div>
                 <div className="bg-blue-500 h-full w-[50%]">
 
