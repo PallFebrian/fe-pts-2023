425e15067af62d6b8c53b4958eb5a7684bf2106b
PallFebrian <pallfebrian9@gmail.com>
2022-11-28 12:56:51 +0700

    pas-nopal2022

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -4,10 +4,9 @@ export default function Login(){
     return(
         <React.Fragment>
             <div className="w-screen h-screen bg-[#4b5ae2]">
-                <div className="bg-red-500 h-full w-[50%]">
-;l;lcmq
+                <div className="bg-[#F9FCF8] h-[100%] w-[50%]">
                 </div>
-                <div className="bg-blue-500 h-full w-[50%]">
+                <div className="bg-[#D6E6D5] h-[100%] w-[50%]">
 
                 </div>
             </div>
