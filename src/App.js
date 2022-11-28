a496edbf4025e41d61c53fa455b48bb7b153e812
PallFebrian <pallfebrian9@gmail.com>
2022-11-28 12:40:16 +0700

    pas-nopal2022

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
  import React from "react";
+import Login from "./pages/login";
 
 function App() {
   return (
     <React.Fragment>
-      <h1 className="bg-red-500">Latihan</h1>
+      {/* <h1 className="bg-red-500">Latihan</h1> */}
+      <Login/>
     </React.Fragment>
   );
 }
---
+++ src/pages/login.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Login(){
+    return(
+        <React.Fragment>
+            <div>
+                <div className="bg-red-500 h-full w-[50%]">
+;l;lcm
+                </div>
+                <div className="bg-blue-500 h-full w-[50%]">
+
+                </div>
+            </div>
+        </React.Fragment>
+    )
+}
